75f12c08c2d3a2d0fe27ed880ef8cd451ac0e862
itzakshada <147384022+itzakshada@users.noreply.github.com>
2024-05-08 23:46:59 -0700

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+void selectionSort()
+int main(){
+  int n;
+  cout<<"enter the number of elements to sort: ";
+  cin>>n;
+  
+  int arr[n];
+  cout<<"enter the elements to be sort:";
+  cin>>arr[n];
+  
+  
 
+
+
+
+
+
+
+
+  
+}

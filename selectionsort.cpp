b53d883fa0eac811092ae1ac6a85cf572ebf81be
itzakshada <147384022+itzakshada@users.noreply.github.com>
2024-05-09 00:26:22 -0700

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -19,10 +19,11 @@ void selectionSort( int arr[],int n)
     }
     cout<<"Sorted elements in ascending order:";
     for(int i=0;i<n;i++)
-    cin>>arr[i];
+    cout<<arr[i]<<"";
     cout<<"Sorted elements in descending order:";
     for(int i=n-1;i>=0;i--)
-    cin>>arr[i];
+    cout<<arr[i]<<"";
+    
   }
 }
 int main()

82a69ec2f157517cbdefa8d5eb92633c66d808b8
itzakshada <147384022+itzakshada@users.noreply.github.com>
2024-05-09 00:19:50 -0700

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -4,23 +4,20 @@ using namespace std;
 void selectionSort( int arr[],int n)
 {
   int i,j,min,temp;
-  for(int i=0;i<n-1;i++){
+  for(int i=0;i<n-1;i++)
+  {
     min=i;
-    for(int j=i+1;j<n;j++){
-      if(arr[j]<arr[min]{
-        min=j;}
+    for(int j=i+1;j<n;j++)
+    {
+      if(arr[j]<arr[min])
+      {
+        min=j;
+      }
     temp=arr[i];
     arr[i]=arr[min];
     arr[min]=temp;
     }
-
-
-
-
-
-
-  
-cout<<"Sorted elements in ascending order:";
+    cout<<"Sorted elements in ascending order:";
 for(int i=0;i<n;i++)
   cin>>arr[i];
 cout<<"Sorted elements in descending order:";

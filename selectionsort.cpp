79297a00b96fc04001fa0325617d5cd3ed2223ff
itzakshada <147384022+itzakshada@users.noreply.github.com>
2024-05-09 00:17:36 -0700

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 void selectionSort( int arr[],int n)
 {
+  int i,j,min,temp;
+  for(int i=0;i<n-1;i++){
+    min=i;
+    for(int j=i+1;j<n;j++){
+      if(arr[j]<arr[min]{
+        min=j;}
+    temp=arr[i];
+    arr[i]=arr[min];
+    arr[min]=temp;
+    }
 
 
 
